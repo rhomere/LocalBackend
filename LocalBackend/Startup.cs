00362d521917678a736f6fd170f91eb7944d150f
Renel Homere <rhomere@gmail.com>
2017-09-11 03:45:52 -0400

    Add project files.

---
+++ LocalBackend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LocalBackend.Startup))]
+
+namespace LocalBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
